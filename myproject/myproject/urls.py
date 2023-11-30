a919dca2b5226c09a272841c18045dbdfa7926c4
projectmiluju <dnjsdyd0712@naver.com>
2023-11-30 09:25:02 +0900

    feat(초기설정): setting.py 에 생성한 앱 추가

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("mysite/", include('mysite.urls'))
+    path("mysite/", include('mysite.urls')),
 ]
