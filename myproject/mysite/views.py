fa32f742225e1e59e19364b2e14f18357a08fbdd
projectmiluju <dnjsdyd0712@naver.com>
2023-11-30 10:06:21 +0900

    feat(초기설정): MainContent 화면출력

--- myproject/mysite/views.py
+++ myproject/mysite/views.py
@@ -1,9 +1,7 @@
-# from django.http import HttpResponse
-
+# from django.http import HttpResponse # 삭제
 from django.shortcuts import render
 from .models import MainContent
-
 def index(request):
-    content_list = MainContent.objects.order_by('-pub-date')
-    context = {'content_list':content_list}
+    content_list = MainContent.objects.order_by('-pub_date')
+    context = {'content_list': content_list}
     return render(request, 'mysite/content_list.html', context)
