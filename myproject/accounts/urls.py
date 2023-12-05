0d7c55ee010e7304de24a918b394ac2ad11df7bc
projectmiluju <dnjsdyd0712@naver.com>
2023-12-05 17:26:39 +0900

    feat(회원가입):

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from. import views
+
 app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'), ]
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('signup/', auth_views.LoginView.as_view(template_name='accounts/signup.html'), name='signup'),
+
+    # path('signup/', views.signup, name='signup'),
+
+]
