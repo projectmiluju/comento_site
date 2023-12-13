b4231b14df90273749ec2b27b7268f44e98683d4
projectmiluju <dnjsdyd0712@naver.com>
2023-12-13 11:39:01 +0900

    feat(회원가입): 회원가입 폼 완성

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -5,10 +5,10 @@ from accounts.forms import SignupForm
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
-        if form.is_vaild():
+        if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect('/')
-        else:
-            form = SignupForm()
-        return render(request, 'accounts/signup.html', {'form': form})
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {'form': form})
