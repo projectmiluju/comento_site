a2647a400334147a3a317c6eb9b3d8c12906d465
projectmiluju <dnjsdyd0712@naver.com>
2023-12-13 20:25:44 +0900

    feat(댓글 기능 구현): 상세 화면에서 댓글 작성 가능

--- myproject/mysite/urls.py
+++ myproject/mysite/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("",views.index),
     path('<int:content_id>/', views.detail, name='detail'),
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
 ]
--- myproject/mysite/views.py
+++ myproject/mysite/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse # 삭제
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import MainContent
+from .forms import CommentForm
 def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
@@ -9,4 +10,21 @@ def index(request):
 def detail(request, content_id):
     content_list = get_object_or_404(MainContent, pk=content_id)
     context = {'content_list': content_list}
+    return render(request, 'mysite/content_detail.html', context)
+
+def comment_create(request, content_id):
+    content_list = get_object_or_404(MainContent, pk=content_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.content_list = content_list
+            comment.author = request.user
+            comment.save()
+            return redirect('detail', content_id=content_list.id)
+    else:
+        form = CommentForm()
+
+    context = {'content_list': content_list, 'form': form}
     return render(request, 'mysite/content_detail.html', context)
