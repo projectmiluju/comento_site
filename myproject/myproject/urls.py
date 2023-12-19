f9b596d83ec8d51ab5ab20ff6213042b3753dcef
projectmiluju <dnjsdyd0712@naver.com>
2023-12-19 18:16:59 +0900

    feat(이미지삽입): 이미지 업로드 및 출력 기능

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # from mysite import views
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     path("", include('pages.urls')),
     path('accounts/', include('accounts.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
